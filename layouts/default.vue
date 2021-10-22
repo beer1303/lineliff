6e7ce4ee51b7f2d4572e0cb37cb006c6aaae2442
beer1303 <beer1303@hotmail.com>
2021-10-22 17:28:59 +0700

    upgit

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <nuxt></nuxt>
+  </v-app>
+</template>
+
+
+<style lang="scss">
+  .v-application{
+    font-family: 'Quicksand', sans-serif;
+    }
+  .v-toolbar__title{
+    text-align: center;
+    width: 100%;
+    font-weight: bold;
+    }
+  .v-application .primary{
+  background-color: #1A56BE !important;
+    border-color: #1A56BE !important;
+    }
+  .text-primary{
+      color: #1A56BE;
+    }
+  .text-title{
+      font: size 20px;
+      font-weight: bold;
+    }
+    .v-form{
+      padding: 0 10px;
+    }
+    .circle{
+      width: 45px;
+      height: 45px;
+      background: #1A56BE;
+      color: #fff;
+      border-radius: 50%;
+    }
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <v-app-bar
+      color="primary"
+      dense
+      flat
+      dark
+      >
+      <v-toolbar-title>Register</v-toolbar-title>
+      </v-app-bar>
+      <v-container class="pt-0 pb-0">
+        <v-row>
+          <v-col cols="12">
+            <div class="mt-7 text-primary text-title text-center">
+              Step 1 of 2
+            </div>
+          </v-col>
+          <v-col cols="12" class="text-center pb-0">
+              <img src=~/assets/img/profile.png alt="" width="155">
+          </v-col>
+          <v-col cols="12" class="text-center pt-2 pb-0">
+            Display Name
+          </v-col>
+          <v-col cols="12">
+            <v-form>
+              <v-text-field
+              v-model="form.firsname"
+              label="FirsName"
+              dense
+              required
+        ></v-text-field>
+        <v-text-field
+              v-model="form.lastname"
+              label="LastName"
+              dense
+              required
+        ></v-text-field>
+        <div class="gender-group">
+          <p>Gender</p>
+          <div class="circle">
+            x
+          </div>
+        </div>
+            </v-form>
+          </v-col>
+        </v-row>
+      </v-container>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+    form:{
+      firsname: '' ,
+      lastname: ''
+        }
+      }
+   }
+  }
+</script>
+
